ab3a7b73e8df9893dd38dcce054df3f18f45a050
Inna <serebryakovainna@mail.ru>
2023-04-18 03:05:51 +0300

    Initial commit

---
+++ src/test/java/pro/sky/hw4user/Hw4UserApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.hw4user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw4UserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
